dfcd39bcf1ba80034b6937755e873634cf7b397f
Ian Matic <lordmatic@yahoo.com>
2019-10-30 09:57:36 -0400

    polish door

--- Assets/Scripts/Objects/Door.cs
+++ Assets/Scripts/Objects/Door.cs
@@ -101,7 +101,7 @@ public class Door : MonoBehaviour
 
     bool NearDoor() // Checks if a player is near the door
     {
-        return (gameObject.transform.position - player.transform.position).magnitude < 3f;
+        return (gameObject.transform.position - player.transform.position).magnitude < 3.5f;
     }
 
     bool EnemyNearDoor()
