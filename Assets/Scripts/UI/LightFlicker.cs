9b70ab7907dd0a068a95f4107c25d3d4f3160d63
Maliik <mab8141@g.rit.edu>
2019-10-30 10:30:52 -0400

    Menu

--- Assets/Scripts/UI/LightFlicker.cs
+++ Assets/Scripts/UI/LightFlicker.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class LightFlicker : MonoBehaviour
 {
     // Start is called before the first frame update
+    public bool isFlickering = false;
     void Start()
     {
-        InvokeRepeating("lightSwitchAttempt", 1.0f, 3.0f);   
+        if (isFlickering)
+        {
+            InvokeRepeating("lightSwitchAttempt", 1.0f, 3.0f);
+        }
+           
     }
     void lightSwitchAttempt()
     {
